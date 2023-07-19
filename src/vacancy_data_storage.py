fe06954461a384327f35ed732fc66cdf9801a648
Natalia Melnik <nataliamelnik138@gmail.com>
2023-07-19 21:23:08 +0600

    Added abstract methods to the abstract class for working with a file

--- src/vacancy_data_storage.py
+++ src/vacancy_data_storage.py
@@ -5,11 +5,14 @@ class VacancyDataStorage(ABC):
     """
     Абстрактный класс для работы с файлом
     """
+    @abstractmethod
     def add_vacancies(self, vacancy):
         pass
 
+    @abstractmethod
     def get_data_from_file(self):
         pass
 
+    @abstractmethod
     def remove_data_from_file(self):
         pass
